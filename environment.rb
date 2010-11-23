a94782098ccd0eb6008d168fbcba527bcb408142
Jason Weathered and Lucas Willett <dev+jason+lucas@ennova.com.au>
2010-11-23 18:07:16 +1000

    Added DataMapper DB config with loading of models.

--- environment.rb
+++ environment.rb
@@ -2,3 +2,7 @@ require 'rubygems'
 require 'bundler'
 
 Bundler.require
+
+DataMapper::Logger.new($stdout, :info)
+Dir.glob('models/*.rb').each { |file| require file }
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/shore")
