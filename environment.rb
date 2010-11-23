015c9c3426f4c2d9900122b38be6a32ec876c9a6
Jason Weathered and Lucas Willett <dev+jason+lucas@ennova.com.au>
2010-11-23 18:33:04 +1000

    Added token authentication.

--- environment.rb
+++ environment.rb
@@ -6,3 +6,5 @@ Bundler.require
 DataMapper::Logger.new($stdout, :info)
 Dir.glob('models/*.rb').each { |file| require file }
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/shore")
+
+$API_TOKEN = ENV['API_TOKEN'] || '12345' # That's the kinda thing an idiot would have on his luggage!
--- shore.rb
+++ shore.rb
@@ -2,11 +2,26 @@ class Shore < Sinatra::Application
   set :haml, :format => :html5
   set :views, 'views'
 
+  helpers do
+    def protected!
+      unless authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Shore API")
+        throw(:halt, [401, "Not authorized\n"])
+      end
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['', $API_TOKEN]
+    end
+  end
+
   get '/' do
     haml :index
   end
 
   get '/shorten' do
+    protected!
     entry = Entry.shorten! params[:url]
     content_type 'text/plain', :charset => 'utf-8'
     "http://#{request.env['HTTP_HOST']}/#{entry.slug}"
